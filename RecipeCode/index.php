098388faf8d97fa98ba721b8e38093c6096a542b
Wen Yu Wang <wen.wang2001@gmail.com>
2024-05-25 01:16:38 +0800

    Wen commit

--- RecipeCode/index.php
+++ RecipeCode/index.php
@@ -1,21 +1,53 @@
+<?php 
+$api_key = '1';
+$categories = fetchCategories();
+?>
+
 <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
-    <label for="ingredients">Enter ingredients (separated by commas):</label><br>
-    <input type="text" id="ingredients" name="ingredients"><br><br>
+    <label for="search">Enter ingredients (separated by commas):</label>
+    <input type="text" id="search" name="search" list="searchList"><br><br>
+    <datalist id="searchList"> 
+        <?php 
+        foreach($categories as $outIng) {
+            echo "<option value='$outIng'>";
+        }
+        ?>
+    </datalist>
     <button type="submit">Search</button>
 </form>
 
 <?php 
-
-$api_key = '1';
+function fetchCategories() {
+    global $api_key;
+    $url = "https://www.themealdb.com/api/json/v1/{$api_key}/list.php?i=list";
+    $response = file_get_contents($url);
+    if ($response) {
+        $data = json_decode($response, true);
+        if (isset($data['meals']) && !empty($data['meals'])) {
+            $categories = array();
+            foreach ($data['meals'] as $meal) {
+                $categories[] = $meal['strIngredient'];
+            }
+            return $categories;
+        } else {
+            return array();
+        }
+    } else {
+        echo "Failed to fetch data from MealDB API.";
+        return false;
+    }
+}
 
 function printIngredients($ings, $recipeIng, $id){
     if (isArraySubset($ings, $recipeIng)) {
+        $recipe = array();
         echo "Ingredients for idMeal {$id}: <br>";
         foreach($recipeIng as $ingre){
             echo "$ingre <br>";
         }
         echo "<br>";
     }
+    return $recipe;
 }
 
 function isArraySubset($array1, $array2) {
@@ -63,10 +95,11 @@ function fetchRecipesByIngredient($ing, $ings) {
     if ($response) {
         $data = json_decode($response, true);
         if (isset($data['meals']) && !empty($data['meals'])) {
+            $recipes = array();
             foreach ($data['meals'] as $recipe) {
                 $idMeal = $recipe['idMeal'];
                 $recipeIngredients = fetchIngredientsByIdMeal($idMeal);
-                printIngredients($ings, $recipeIngredients, $idMeal);
+                array_push($recipes, printIngredients($ings, $recipeIngredients, $idMeal));
             }
         } else {
             echo "No recipes found for the given ingredient.";
@@ -100,13 +133,20 @@ function fetchIngredientsByIdMeal($idMeal) {
         echo "Failed to fetch data from MealDB API.";
     }
     return NULL;
+
+
 }
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $inputIngredients = isset($_POST['ingredients']) ? $_POST['ingredients'] : '';
+    $inputIngredients = isset($_POST["search"]) ? $_POST["search"]: '';
     $ingredients = explode(',', $inputIngredients);
     $ingredients = array_map('trim', $ingredients);
-    foreach ($ingredients as $ingredient) {
+    foreach($ingredients as $ingredient) {
+        if(in_array(strtolower($ingredient), array_map('strtolower', $categories))) {
+            echo "<p>Search term '$ingredient' found!</p>";
+        } else {
+            echo "<p>Search term '$ingredient' not found! Please select from the list.</p>";
+        }
         fetchRecipesByIngredient($ingredient, $ingredients);
     }
 }
