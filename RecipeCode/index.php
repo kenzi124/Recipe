38cbb14738a13c868d250ccfe6ea0085c19919a6
Wen Yu Wang <wen.wang2001@gmail.com>
2024-05-25 20:55:34 +0800

    Wen commit

--- RecipeCode/index.php
+++ RecipeCode/index.php
@@ -1,7 +1,8 @@
-<?php
+<?php 
 include 'functions.php';
 $categories = fetchCategories();
 $enteredIngredients = isset($_POST['enteredIngredients']) ? json_decode($_POST['enteredIngredients'], true) : [];
+
 if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['action'])) {
     include 'process.php';
 }
@@ -19,6 +20,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['action'])) {
     <label for="search">Enter ingredients:</label>
     <input type="text" id="search" name="search" list="searchList">
     <button type="submit" name="action" value="addIngredient">Add</button>
+    <button type="submit" name="action" value="searchRecipes">Search</button>
     <datalist id="searchList">
         <?php 
         foreach($categories as $outIng) {
@@ -26,20 +28,24 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['action'])) {
         }
         ?>
     </datalist>
-    <div id="ingredientButtons">
-        <?php
-        foreach($enteredIngredients as $index => $name) {
-            echo '<button type="button" id="button' . $index . '" onclick="removeButton(\'button' . $index . '\', \'' . $name . '\')">' . $name . '</button>';
-        }
-        ?>
-    </div>
     <input type="hidden" name="enteredIngredients" value="<?php echo htmlspecialchars(json_encode($enteredIngredients)); ?>">
 </form>
 
-<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
-    <input type="hidden" name="enteredIngredients" value="<?php echo htmlspecialchars(json_encode($enteredIngredients)); ?>">
-    <button type="submit" name="action" value="searchRecipes">Search</button>
-</form>
+<div id="recipeResults">
+    <?php
+    if ($_SERVER["REQUEST_METHOD"] == "POST" && $_POST['action'] == 'searchRecipes') {
+        fetchRecipes($enteredIngredients);
+    }
+    ?>
+</div>
+
+<div id="ingredientButtons">
+    <?php
+    foreach($enteredIngredients as $index => $name) {
+        echo '<button type="button" id="button' . $index . '" onclick="removeButton(\'button' . $index . '\', \'' . $name . '\')">' . $name . '</button>';
+    }
+    ?>
+</div>
 
 <form method="post" id="removeIngredientForm" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
     <input type="hidden" name="action" value="removeIngredient">
