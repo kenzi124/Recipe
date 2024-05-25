1564c68d349c8f84139928a952e8760604b955a4
Wen Yu Wang <wen.wang2001@gmail.com>
2024-05-26 00:30:10 +0800

    Wen commit

--- RecipeCode/process.php
+++ RecipeCode/process.php
@@ -1,11 +1,11 @@
 <?php
-if ($_POST['action'] == 'addIngredient') {
-    addIngredient($enteredIngredients, $categories);
-} elseif ($_POST['action'] == 'searchRecipes') {
-    fetchRecipes($enteredIngredients);
-} elseif ($_POST['action'] == 'removeIngredient') {
-    removeIngredient($enteredIngredients);
+if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['action'])) {
+    if ($_POST['action'] == 'addIngredient') {
+        addIngredient($enteredIngredients, $categories);
+    } elseif ($_POST['action'] == 'searchRecipes') {
+        fetchRecipes($enteredIngredients);
+    } elseif ($_POST['action'] == 'removeIngredient') {
+        removeIngredient($enteredIngredients);
+    }
 }
 ?>
-
-<input type="hidden" name="enteredIngredients" value="<?php echo htmlspecialchars(json_encode($enteredIngredients)); ?>">
