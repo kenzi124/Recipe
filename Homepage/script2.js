c4d831d5e37ca111cac4044188c7f5b807fd75e7
danifabro <danialekzandr@gmail.com>
2024-05-30 20:28:01 +0800

    Create script2.js

---
+++ Homepage/script2.js
@@ -0,0 +1,24 @@
+if (lightModeToggle) {
+        lightModeToggle.addEventListener('click', () => {
+            const newLightMode = document.body.classList.toggle('light-mode2');
+            localStorage.setItem('lightMode', newLightMode);
+        });
+    } else {
+        console.error('Light mode toggle button not found');
+    }
+
+    function toggleLightMode(isLightMode) {
+        if (isLightMode) {
+            document.body.classList.add('light-mode');
+        } else {
+            document.body.classList.remove('light-mode');
+        }
+    }
+
+document.getElementById('ingredSearch').addEventListener('click', function() {
+  window.location.href = 'ingredients.html';
+});
+
+document.getElementById('dishSearch').addEventListener('click', function() {
+  window.location.href = 'dish.html';
+});
