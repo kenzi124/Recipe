043c8f2a41139823b3c1a5b162e0b270ec9848b9
danifabro <danialekzandr@gmail.com>
2024-06-08 22:13:53 +0800

    Update RecipeName.php

--- RecipeCode/RecipeName.php
+++ RecipeCode/RecipeName.php
@@ -2,14 +2,26 @@
 <html>
 <head>
     <title>Recipe Finder By Name</title>
-    <script src="script.js"></script>
+    <script src="script3.js"></script>
+    <link rel="stylesheet" href="style3.css">
+    <link rel ="stylesheet" href="light-mode3.css">
 </head>
 <body>
+  <main>
     <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
-        <label for="search">Enter recipe name:</label>
+        <div>
+        <label for="search"> Enter recipe name:</label>
+        </div>
+        
         <input type="text" id="search" name="search">
+        <div>
+        <br>
+        <br>
         <button type="submit" name="action" value="searchRecipes">Search</button>
+        <button type="button" id="lightModeToggle" class="btmright">Light Mode</button>
+        </div>
     </form>
+  </main>
 </body>
 </html>
 
