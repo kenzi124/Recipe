624f8056ccbc4d58873b1283df0e6c343ccfe8f3
Arczodiak <gilbraith.liucaesar@gmail.com>
2024-06-08 18:13:17 +0800

    G

--- LoginPage/includes/config.php
+++ LoginPage/includes/config.php
@@ -1,8 +1,10 @@
 <?php
-
-$hostName = "localhost";
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+$hostName = "db";
 $dbUser = "root";
-$dbPassword = "";
+$dbPassword = "billybob";
 $dbName = "login_register";
 $conn = mysqli_connect($hostName, $dbUser, $dbPassword, $dbName);
 if (!$conn) {
