520f26c8111beb83e0d1d734228aa59da836586e
kenzi <boyplay124b@gmail.com>
2024-06-10 17:07:06 +0800

    fixed

--- LoginPage/includes/config.php
+++ LoginPage/includes/config.php
@@ -2,9 +2,9 @@
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
-$hostName = "db";
+$hostName = "localhost";
 $dbUser = "root";
-$dbPassword = "billybob";
+$dbPassword = "";
 $dbName = "login_register";
 $conn = mysqli_connect($hostName, $dbUser, $dbPassword, $dbName);
 if (!$conn) {
